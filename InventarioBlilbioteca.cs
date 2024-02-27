e8d8b9f858d7083053ba6d0afcc87b5ad2483f04
Usuario <Usuario@B9-203-20026>
2024-02-27 13:06:48 -0500

    Agregar archivos de proyecto.

---
+++ InventarioBlilbioteca.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerSgbSantiagoEchavarriaP
+{
+    class InventarioBlilbioteca
+    {
+        string[,] Libro = new string[100,4];
+        bool[] CantidadLibro = new bool[100];
+        string[] EstadoLibro = new string[100];
+        int i,aux;
+        string BuscaNombre, BuscaCodigo;
+        public void VerificacionEstadoCodigo()
+        {
+            for (i = 0; i < 100; i++)
+            {
+                if (Libro[i, 0] == BuscaNombre)
+                {
+                    i = aux;
+                }
+            }
+            if (EstadoLibro[aux] == "Disponible")
+            {
+                Console.WriteLine("El libro esta Disponible");
+            }
+            else if (EstadoLibro[aux] == " ")
+            {
+                Console.WriteLine("Libro No Disponible");
+            }
+        }
+        public void VerificacionEstadoNombre()
+        {
+            for(i=0;i<100;i++)
+            {
+                if(Libro[i,1]==BuscaCodigo)
+                {
+                    i = aux;
+                }
+            }
+            if(EstadoLibro[aux]=="Disponible")
+            {
+                Console.WriteLine("El libro esta Disponible");
+            }
+            else if(EstadoLibro[aux]==" ")
+            {
+                Console.WriteLine("Libro No Disponible");
+            }
+        }
+        public void Control_Inventario()
+        {
+
+        }
+
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerSgbSantiagoEchavarriaP
+{
+    class Usuario
+    {
+        string[,] Usuarios = new string[100, 4];
+        public void CrearUsuario()
+        {
+
+        }
+        public void ModificarUsusario()
+        {
+
+        }
+    }
+}
